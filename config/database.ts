8235029d8a6a84931c577eda652ba6f102b160b3
bob <weeiwaai02@gmail.com>
2023-08-13 20:01:42 +0930

    database setup

--- config/database.ts
+++ config/database.ts
@@ -6,7 +6,6 @@
  */
 
 import Env from '@ioc:Adonis/Core/Env'
-import Application from '@ioc:Adonis/Core/Application'
 import type { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
 const databaseConfig: DatabaseConfig = {
@@ -25,34 +24,31 @@ const databaseConfig: DatabaseConfig = {
   connections: {
     /*
     |--------------------------------------------------------------------------
-    | SQLite
+    | MySQL config
     |--------------------------------------------------------------------------
     |
-    | Configuration for the SQLite database.  Make sure to install the driver
+    | Configuration for MySQL database. Make sure to install the driver
     | from npm when using this connection
     |
-    | npm i sqlite3
+    | npm i mysql2
     |
     */
-    sqlite: {
-      client: 'sqlite',
+    mysql: {
+      client: 'mysql',
       connection: {
-        filename: Application.tmpPath('db.sqlite3'),
-      },
-      pool: {
-        afterCreate: (conn, cb) => {
-          conn.run('PRAGMA foreign_keys=true', cb)
-        }
+        host: Env.get('DB_HOST', '127.0.0.1'),
+        port: Env.get('DB_PORT', '3306'),
+        user: Env.get('DB_USER', 'root'),
+        password: Env.get('DB_PASSWORD', 'password'),
+        database: Env.get('DB_DATABASE', 'lucid'),
       },
       migrations: {
         naturalSort: true,
       },
-      useNullAsDefault: true,
       healthCheck: false,
       debug: false,
     },
-
-  }
+  },
 }
 
 export default databaseConfig
